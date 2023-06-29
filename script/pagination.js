b286c7a4d7b0ba8682a53d800b1e5a2bc1a57b42
Ariel Szabó <arielszabobusiness@gmail.com>
2023-06-29 11:48:55 +0200

    commit how to get quantity/size of companies

--- script/pagination.js
+++ script/pagination.js
@@ -10,12 +10,21 @@ function updateTable(pageIndex) {
       }&maxage=300`
     );
 
+  // const urlQuantityOfCompanies =
+  // "https://corsproxy.io/?" +
+  // encodeURIComponent(
+  //   `https://jobboerse.htl-braunau.at/htl_job_api.php?cmd=getcpylist&count=${quantityOfCompaniesPerSite}&from=${
+  //     pageIndex * quantityOfCompaniesPerSite
+  //   }&maxage=300`
+  // );
+
   fetch(url)
     .then((data) => {
       return data.json();
     })
     .then((objectData) => {
       // console.log(objectData);
+      console.log(objectData.resultset);
       let tableData = "";
       objectData.resultset.map((values) => {
         tableData += `
@@ -41,41 +50,74 @@ let linkElements = document.getElementsByClassName("link");
 const prevBtnElement = document.querySelector(".prev-btn");
 const nextBtnElement = document.querySelector(".next-btn");
 
-let currentValue = 1;
+// let currentValue = 1;
 
-for (lElement of linkElements) {
-  lElement.addEventListener("click", (event) => {
-    for (lElement of linkElements) {
-      lElement.classList.remove("active");
-    }
-    event.target.classList.add("active");
-    currentValue = event.target.value;
-  });
-}
+// for (lElement of linkElements) {
+//   lElement.addEventListener("click", (event) => {
+//     for (lElement of linkElements) {
+//       lElement.classList.remove("active");
+//     }
+//     event.target.classList.add("active");
+//     currentValue = event.target.value;
+//   });
+// }
 
 prevBtnElement.addEventListener("click", () => {
   if (pageIndex > 0) {
-    for (lElement of linkElements) {
-      lElement.classList.remove("active");
-    }
-      // TODO: Page index
-      pageIndex--;
-      updateTable(pageIndex);
-      linkElements[currentValue - 1].classList.add("active");
-      currentValue--;
-    }
+    // for (lElement of linkElements) {
+    //   lElement.classList.remove("active");
+    // }
+    // TODO: Page index
+    pageIndex--;
+    updateTable(pageIndex);
+    // linkElements[currentValue - 1].classList.add("active");
+    // currentValue = pageIndex - 1;
+    // currentValue--;
+  }
 });
 
 nextBtnElement.addEventListener("click", () => {
-  if (pageIndex < linkElements.length) { // unendliche zahl!!
-    for (lElement of linkElements) {
-      lElement.classList.remove("active");
-    }
+  if (pageIndex < 100) {
+    // linkElements.length unendliche zahl!!
+    // for (lElement of linkElements) {
+    //   lElement.classList.remove("active");
+    // }
     // currentValue++;
     // TODO: Page index
     pageIndex++;
     updateTable(pageIndex);
-    linkElements[currentValue - 1].classList.add("active");
-    currentValue++;
+    // linkElements[currentValue - 1].classList.add("active");
+    // currentValue++;
+    // currentValue = pageIndex;
   }
 });
+
+// fetch: QuantityOfCompanies
+
+const url2 =
+  "https://corsproxy.io/?" +
+  encodeURIComponent(
+    `https://jobboerse.htl-braunau.at/htl_job_api.php?cmd=getcpysize&maxage=300`
+  );
+
+fetch(url2)
+  .then((data) => {
+    return data.json();
+  })
+  .then((objectData) => {
+    console.log("Quantity of companies: " + objectData.size);
+    let tableData = "";
+    objectData.resultset.map((values) => {
+      tableData += `
+      <tr class="tr-table-body">
+      <td>${values.name}</td>
+      <td>${values.zip} ${values.city} - ${values.country}</td>
+      <td>Jobs: ${values.jobs}</td>
+      <td><button class="more-info-btn">More info</button></td>
+      <ul class="ul-opened-by-button"></ul>
+      </tr>`;
+      });
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = tableData;
+  })
+  .catch((error) => console.log(error));
