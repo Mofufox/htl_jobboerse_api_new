592d184f5956dfcccfb501effe3029ffeaa61cb0
Ariel Szabó <arielszabobusiness@gmail.com>
2023-06-30 15:48:37 +0200

    Per buttonclick the more infos appear, yet the companies appear in a random way.

--- script/pagination.js
+++ script/pagination.js
@@ -1,4 +1,4 @@
-const quantityOfCompaniesPerSite = 100;
+const quantityOfCompaniesPerSite = 10;
 let quantityOfCompanies = 0;
 let pageIndex = 1;
 
@@ -38,26 +38,190 @@ function updateTable(pageIndex) {
       objectDataCompanies = objectData.resultset;
       console.log(objectDataCompanies);
 
-      let tableData = "";
-      objectData.resultset.map((values) => {
-        console.log(values.nr + "." + values.name);
-        console.log("Page index: " + pageIndex);
-        tableData += `
-          <tr class="tr-table-body">
-            <td>${values.name}</td>
-            <td>${values.zip} ${values.city} - ${values.country}</td>
-            <td>Jobs: ${values.jobs}</td>
-            <td><button class="more-info-btn" value="${values.nr}" data-moreinfobtn="${values.nr}">More info</button></td>
-          </tr>
-          `;
-      });
+      // TODO:
+      for (let i = 0; i < objectDataCompanies.length; i++) {
+        let company_id = objectDataCompanies[i].company_id;
 
-      // <ul class="ul-opened-by-button"><li>Ariel</li></ul>
-      const tableBody = document.getElementById("table-body");
-      tableBody.innerHTML = tableData;
+        const urlCompany =
+          "https://corsproxy.io/?" +
+          encodeURIComponent(
+            `https://jobboerse.htl-braunau.at/htl_job_api.php?cmd=getcpysingle&company_id=${company_id}`
+          );
 
-      const moreInfoBtnElements = document.querySelectorAll(".more-info-btn");
-      console.log(moreInfoBtnElements);
+        fetch(urlCompany)
+          .then((data) => {
+            return data.json(); // converted to object
+          })
+          .then((companyData) => {
+            // Process company data here
+            console.log(companyData);
+            companyData = companyData.result;
+            console.log(companyData);
+            console.log(companyData.country);
+
+            // TODO: HIER!!!
+            let tableData = "";
+            objectData.resultset.map((values) => {
+              // console.log(values.nr + "." + values.name);
+              console.log(values.nr + "." + values.country); //Nicht jede Frima hat eine country eingetragen!!??
+              console.log("Page index: " + pageIndex);
+              tableData += `
+                <tr class="tr-table-body">
+                  <td>${values.name}</td>
+                  <td>${values.zip} ${values.city} - ${values.country}</td>
+                  <td>Jobs: ${values.jobs}</td>
+                  <td><button class="more-info-btn" value="${values.nr}" data-moreinfobtn="${values.nr}">More info</button></td>
+                </tr>
+                `;
+            });
+
+            // <ul class="ul-opened-by-button"><li>Ariel</li></ul>
+            const tableBody = document.getElementById("table-body"); //TODO: brauche ich das????
+            tableBody.innerHTML = tableData;
+
+            const moreInfoBtnElements =
+              document.querySelectorAll(".more-info-btn");
+            // console.log(moreInfoBtnElements);
+
+            for (moreInfoBtnEl of moreInfoBtnElements) {
+              moreInfoBtnEl.addEventListener("click", (event) => {
+                const clickedButton = event.target;
+
+                const additionalRows = [];
+                const labels = [
+                  "Address",
+                  "Company ID",
+                  "Companygroup ID",
+                  "Email",
+                  "Fax",
+                  "Image",
+                  "Name",
+                  "Phone",
+                  "Website",
+                ];
+                const fields = [
+                  `${companyData.zip} ${companyData.city} - ${companyData.country}`,
+                  `${companyData.company_id}`,
+                  `${companyData.companygroup_id}`,
+                  `${companyData.email}`,
+                  `${companyData.fax}`,
+                  `${companyData.image}`,
+                  `${companyData.name}`,
+                  `${companyData.phone}`,
+                  `${companyData.www}`,
+                  `${companyData.email}`,
+                ];
+
+                for (let i = 0; i < labels.length; i++) {
+                  const additionalRow = document.createElement("tr");
+                  additionalRow.classList.add("additional-row"); 
+             
+
+                  const thElement = document.createElement("th");
+                  const tdElement1 = document.createElement("td");
+                  const tdElement2 = document.createElement("td");
+                  const tdElement3 = document.createElement("td");
+
+                  thElement.textContent = labels[i];
+                  tdElement1.textContent = fields[i];
+                  tdElement2.textContent = "";
+                  tdElement3.textContent = "";
+
+                  additionalRow.appendChild(thElement);
+                  additionalRow.appendChild(tdElement1);
+                  additionalRow.appendChild(tdElement2);
+                  additionalRow.appendChild(tdElement3);
+
+                  additionalRows.push(additionalRow);
+                  // thElement.style.background = "blue";
+                }
+
+                additionalRows.reverse().forEach((row) => {
+                  clickedButton.parentElement.parentElement.insertAdjacentElement(
+                    "afterend",
+                    row
+                  );
+                });
+              });
+            }
+
+            // for (moreInfoBtnEl of moreInfoBtnElements) {
+            //   moreInfoBtnEl.addEventListener("click", (event) => {
+            //     const clickedButton = event.target;
+
+            //     const additionalRow1 = document.createElement("tr");
+            //     additionalRow1.classList.add("additional-row");
+
+            //     const additionalRow2 = document.createElement("tr");
+            //     additionalRow2.classList.add("additional-row");
+
+            //     const thElement1 = document.createElement("th");
+            //     const tdElement1 = document.createElement("td");
+            //     const tdElement2 = document.createElement("td");
+            //     const tdElement3 = document.createElement("td");
+
+            //     const thElement2 = document.createElement("th");
+            //     const tdElement4 = document.createElement("td");
+            //     const tdElement5 = document.createElement("td");
+            //     const tdElement6 = document.createElement("td");
+
+            //     // tdElement.setAttribute("colspan", "4");
+            //     thElement1.textContent = `Adresse`;
+            //     tdElement1.textContent = `${companyData.zip} ${companyData.city}`;
+            //     tdElement2.textContent = `${companyData.country}`;
+            //     tdElement3.textContent = "";
+
+            //     thElement2.textContent = `Company ID`;
+            //     tdElement4.textContent = `${companyData.company_id}`;
+            //     tdElement5.textContent = ``;
+            //     tdElement6.textContent = "";
+
+            //     additionalRow1.appendChild(thElement1);
+            //     additionalRow1.appendChild(tdElement1);
+            //     additionalRow1.appendChild(tdElement2);
+            //     additionalRow1.appendChild(tdElement3);
+
+            //     additionalRow2.appendChild(thElement2);
+            //     additionalRow2.appendChild(tdElement4);
+            //     additionalRow2.appendChild(tdElement5);
+            //     additionalRow2.appendChild(tdElement6);
+
+            //     console.log(clickedButton);
+            //     clickedButton.parentElement.parentElement.insertAdjacentElement(
+            //       "afterend",
+            //       additionalRow2
+            //     );
+            //     clickedButton.parentElement.parentElement.insertAdjacentElement(
+            //       "afterend",
+            //       additionalRow1
+            //     );
+            //   });
+            // }
+          })
+          .catch((error) => console.log(error));
+      }
+      // TODO:
+
+      // let tableData = "";
+      // objectData.resultset.map((values) => {
+      //   console.log(values.nr + "." + values.name);
+      //   console.log("Page index: " + pageIndex);
+      //   tableData += `
+      //     <tr class="tr-table-body">
+      //       <td>${values.name}</td>
+      //       <td>${values.zip} ${values.city} - ${values.country}</td>
+      //       <td>Jobs: ${values.jobs}</td>
+      //       <td><button class="more-info-btn" value="${values.nr}" data-moreinfobtn="${values.nr}">More info</button></td>
+      //     </tr>
+      //     `;
+      // });
+
+      // // <ul class="ul-opened-by-button"><li>Ariel</li></ul>
+      // const tableBody = document.getElementById("table-body");
+      // tableBody.innerHTML = tableData;
+
+      // const moreInfoBtnElements = document.querySelectorAll(".more-info-btn");
+      // console.log(moreInfoBtnElements);
 
       // for (moreInfoBtnEl of moreInfoBtnElements) {
       //   moreInfoBtnEl.addEventListener("click", (event) => {
@@ -72,7 +236,7 @@ function updateTable(pageIndex) {
       //     const tdElement3 = document.createElement("td");
 
       //     // tdElement.setAttribute("colspan", "4");
-      //     thElement.textContent = `NAME`;
+      //     thElement.textContent = `NAME ${companyData.www}`;
       //     tdElement1.textContent = "HALOOOOOO";
       //     tdElement2.textContent = "";
       //     tdElement3.textContent = "";
@@ -87,90 +251,8 @@ function updateTable(pageIndex) {
       //       "afterend",
       //       additionalRow
       //     );
-
-
-
-
-
-          // Last fetch
-          for (let i = 0; i < objectDataCompanies.length; i++) {
-            let company_id = objectDataCompanies[i].company_id;
-
-            const urlCompany =
-              "https://corsproxy.io/?" +
-              encodeURIComponent(
-                `https://jobboerse.htl-braunau.at/htl_job_api.php?cmd=getcpysingle&company_id=${company_id}`
-              );
-
-            fetch(urlCompany)
-              .then((data) => {
-                return data.json(); // converted to object
-              })
-              .then((objectData) => {
-                //   moreInfoBtn.addEventListener("click", () => {
-                const values = objectData.result;
-                console.log(values); //DAS passt noch!!
-
-
-
-                for (moreInfoBtnEl of moreInfoBtnElements) {
-                  moreInfoBtnEl.addEventListener("click", (event) => {
-                    const clickedButton = event.target;
-          
-                    const additionalRow = document.createElement("tr");
-                    additionalRow.classList.add("additional-row");
-          
-                    const thElement = document.createElement("th");
-                    const tdElement1 = document.createElement("td");
-                    const tdElement2 = document.createElement("td");
-                    const tdElement3 = document.createElement("td");
-          
-                    // tdElement.setAttribute("colspan", "4");
-                    thElement.textContent = "a";
-                    tdElement1.textContent = "HALOOOOOO";
-                    tdElement2.textContent = "";
-                    tdElement3.textContent = "";
-          
-                    additionalRow.appendChild(thElement);
-                    additionalRow.appendChild(tdElement1);
-                    additionalRow.appendChild(tdElement2);
-                    additionalRow.appendChild(tdElement3);
-          
-                    console.log(clickedButton);
-                    clickedButton.parentElement.parentElement.insertAdjacentElement(
-                      "afterend",
-                      additionalRow
-                    );
-
-
-
-
-
-              //   for (let i = 0; i < objectDataCompanies.length; i++) {
-              //     ulOpenedByButton.appendChild(liElement).innerHTML = `
-              // <table>
-              // <tbody>             
-              //  <tr class="tr-table-body">
-              //  <td> ${values.address}</td>
-              //  <td> ${values.zip} ${values.city} - ${values.country}</td>
-              //  <td> ${values.company_id}</td>
-              //  <td> ${values.companygroup_id}</td>
-              //  <td> ${values.email} ${values.fax}</td>
-              //  <td> ${values.image}</td>
-              //  <td> ${values.name}</td>
-              //  <td> ${values.phone}</td>
-              //  <td> ${values.www}</td>
-              // </tr>
-              // </tbody>
-              // </table>
-              // `;
-              //   }
-                //   });
-              })
-              .catch((error) => console.log(error));
-          }
-        });
-      }
+      //   });
+      // }
     })
     .catch((error) => console.log(error));
 }
