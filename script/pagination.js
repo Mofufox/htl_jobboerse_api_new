c2b280f45cb0a56c495fad9a249ceb0f386e4226
Ariel Szabó <arielszabobusiness@gmail.com>
2023-07-03 11:49:37 +0200

    Task finished

--- script/pagination.js
+++ script/pagination.js
@@ -15,16 +15,12 @@ function updateTable(pageIndex) {
       `https://jobboerse.htl-braunau.at/htl_job_api.php?cmd=getcpysize&maxage=300`
     );
 
-  Promise.all([fetch(urlQuantityOfCompanies), fetch(urlCompanies)])
+  Promise.all([fetch(urlQuantityOfCompanies), fetch(urlCompanies)]) //pending, fulfilled or rejected
     .then((responses) => Promise.all(responses.map((res) => res.json())))
     .then(([quantityData, companiesData]) => {
       quantityOfCompanies = quantityData.size;
       const objectDataCompanies = companiesData.resultset;
 
-      console.log(objectDataCompanies);
-      console.log(objectDataCompanies.length);
-
-      // TODO:
       const fetchCompanyData = (company_id) => {
         const urlCompany =
           "https://corsproxy.io/?" +
@@ -35,9 +31,7 @@ function updateTable(pageIndex) {
         return fetch(urlCompany)
           .then((data) => data.json())
           .then((companyData) => {
-            console.log(companyData);
             companyData = companyData.result;
-            console.log(companyData);
             return companyData;
           });
       };
@@ -48,18 +42,15 @@ function updateTable(pageIndex) {
 
       Promise.all(fetchCompanyDataPromises)
         .then((companyDataArray) => {
-          console.log(companyDataArray);
 
           let tableData = "";
           objectDataCompanies.forEach((values, index) => {
-            console.log(values.nr + "." + values.country);
-            console.log("Page index: " + pageIndex);
             tableData += `
               <tr class="tr-table-body">
                 <td>${values.name}</td>
                 <td>${values.zip} ${values.city} - ${values.country}</td>
                 <td>Jobs: ${values.jobs}</td>
-                <td><button class="more-info-btn" value="${values.nr}" data-moreinfobtn="${values.nr}">More info</button></td>
+                <td><button class="more-info-btn">More info</button></td>
               </tr>
               `;
           });
@@ -70,7 +61,6 @@ function updateTable(pageIndex) {
           const moreInfoBtnElements =
             document.querySelectorAll(".more-info-btn");
 
-          // TODO:
           const fetchJobsDataPerCompany = (company_id) => {
             const urlCompany =
               "https://corsproxy.io/?" +
@@ -81,126 +71,122 @@ function updateTable(pageIndex) {
             return fetch(urlCompany)
               .then((data) => data.json())
               .then((jobsDataPerCompany) => {
-                console.log(jobsDataPerCompany);
-                jobsDataPerCompany = jobsDataPerCompany.result;
-                console.log(jobsDataPerCompany);
+                jobsDataPerCompany = jobsDataPerCompany.resultset;
                 return jobsDataPerCompany;
               });
           };
 
           const fetchJobsDataPerCompanyPromises = objectDataCompanies.map(
-            (company) => fetchJobsDataPerCompany(company.company_id)
+            (company) =>
+              fetchJobsDataPerCompany(company.company_id).catch(() => [])
           );
 
-          console.log(fetchJobsDataPerCompanyPromises);
-
           Promise.all(fetchJobsDataPerCompanyPromises).then(
-            (JobsDataPerCompanyArray) => {
-              console.log(JobsDataPerCompanyArray);
+            (jobsDataPerCompanyArray) => {
 
-              let tableData = "";
-              objectDataCompanies.forEach((values, index) => {
-                console.log(values.nr + "." + values.country);
-                console.log("Page index: " + pageIndex);
-                tableData += `
-                <tr class="tr-table-body">
-                  <td>${values.name}</td>
-                  <td>${values.zip} ${values.city} - ${values.country}</td>
-                  <td>Jobs: ${values.jobs}</td>
-                  <td><button class="more-info-btn" value="${values.nr}" data-moreinfobtn="${values.nr}">More info</button></td>
-                </tr>
-                `;
+              moreInfoBtnElements.forEach((moreInfoBtnEl, index) => {
+                let isDataVisible = false; // Flag to track data visibility
+
+                moreInfoBtnEl.addEventListener("click", displayMoreInfo);
+                function displayMoreInfo(event) {
+                  //Hier habe ich dewegen eine non-arrow function, weil diese auch vor creation aufrufbar ist
+                  const clickedButton = event.target;
+                  const buttonValue = clickedButton.value;
+
+                  const companyData = companyDataArray[index];
+                  const jobsDataPerCompany = jobsDataPerCompanyArray[index];
+
+                  // Process the companyData and jobsDataPerCompany here
+
+                  const additionalRows = [];
+                  const labels = [
+                    "Address",
+                    "Company ID",
+                    "Companygroup ID",
+                    "Email",
+                    "Fax",
+                    "Image",
+                    "Name",
+                    "Phone",
+                    "Website",
+                  ];
+                  const fields = [
+                    `${companyData.address}`,
+                    `${companyData.company_id}`,
+                    `${companyData.companygroup_id}`,
+                    `${companyData.email}`,
+                    `${companyData.fax}`,
+                    `${companyData.image}`,
+                    `${companyData.name}`,
+                    `${companyData.phone}`,
+                    `${companyData.www}`,
+                  ];
+
+                  for (let i = 0; i < labels.length; i++) {
+                    const field = fields[i];
+                    if (field) {
+                      const additionalRow = document.createElement("tr");
+                      additionalRow.classList.add("additional-row");
+
+                      const thElement = document.createElement("th");
+                      const tdElement1 = document.createElement("td");
+                      const tdElement2 = document.createElement("td");
+                      const tdElement3 = document.createElement("td");
+
+                      thElement.textContent = labels[i];
+                      tdElement1.textContent = fields[i];
+                      tdElement2.textContent = "";
+                      tdElement3.innerHTML = "";
+
+                      additionalRow.appendChild(thElement);
+                      additionalRow.appendChild(tdElement1);
+                      additionalRow.appendChild(tdElement2);
+                      additionalRow.appendChild(tdElement3);
+
+                      additionalRows.push(additionalRow);
+                    }
+                  }
+
+                  jobsDataPerCompany.forEach((job, jobIndex) => {
+                    if (isDataVisible) {
+                      return;
+                    }
+
+                    const additionalRow = document.createElement("tr");
+                    additionalRow.classList.add("additional-row-jobs");
+
+                    const thElement = document.createElement("th");
+                    const tdElement1 = document.createElement("td");
+                    const tdElement2 = document.createElement("td");
+                    const tdElement3 = document.createElement("td");
+
+                    thElement.textContent = `Job-Nr. ${jobIndex + 1}`;
+                    tdElement1.textContent = job.title;
+                    tdElement2.textContent = "";
+                    tdElement3.textContent = "";
+
+                    additionalRow.appendChild(thElement);
+                    additionalRow.appendChild(tdElement1);
+                    additionalRow.appendChild(tdElement2);
+                    additionalRow.appendChild(tdElement3);
+
+                    additionalRows.push(additionalRow);
+                  });
+
+                  additionalRows.reverse().forEach((row) => {
+                    if (isDataVisible) {
+                      return;
+                    }
+                    clickedButton.parentElement.parentElement.insertAdjacentElement(
+                      "afterend",
+                      row
+                    );
+                  });
+                  isDataVisible = true;
+                }
               });
             }
           );
-
-
-
-
-
-
-
-
-          moreInfoBtnElements.forEach((moreInfoBtnEl, index) => {
-            let isDataVisible = false; // Flag to track data visibility
-
-            moreInfoBtnEl.addEventListener("click", (event) => {
-              const clickedButton = event.target;
-              const buttonValue = clickedButton.value;
-
-              const companyData = companyDataArray[index];
-
-              // Process the companyData here
-              console.log(companyData);
-              console.log(companyData.address);
-              console.log(companyData.name);
-              console.log(companyData.phone);
-              console.log(companyData.www);
-              console.log(companyData.email);
-
-              const additionalRows = [];
-              const labels = [
-                "Address",
-                "Company ID",
-                "Companygroup ID",
-                "Email",
-                "Fax",
-                "Image",
-                "Name",
-                "Phone",
-                "Website",
-              ];
-              const fields = [
-                // `${companyData.zip} ${companyData.city} - ${companyData.country}`,
-                `${companyData.address}`,
-                `${companyData.company_id}`,
-                `${companyData.companygroup_id}`,
-                `${companyData.email}`,
-                `${companyData.fax}`,
-                `${companyData.image}`,
-                `${companyData.name}`,
-                `${companyData.phone}`,
-                `${companyData.www}`,
-                `${companyData.email}`,
-              ];
-
-              // console.log(company_id);
-              // console.log(`${companyData.companygroup_id}`);
-
-              for (let i = 0; i < labels.length; i++) {
-                const additionalRow = document.createElement("tr");
-                additionalRow.classList.add("additional-row");
-
-                const thElement = document.createElement("th");
-                const tdElement1 = document.createElement("td");
-                const tdElement2 = document.createElement("td");
-                const tdElement3 = document.createElement("td");
-
-                thElement.textContent = labels[i];
-                tdElement1.textContent = fields[i];
-                tdElement2.textContent = "";
-                tdElement3.textContent = "";
-
-                additionalRow.appendChild(thElement);
-                additionalRow.appendChild(tdElement1);
-                additionalRow.appendChild(tdElement2);
-                additionalRow.appendChild(tdElement3);
-
-                additionalRows.push(additionalRow);
-              }
-
-              additionalRows.reverse().forEach((row) => {
-                if (isDataVisible) {
-                  return;
-                }
-                clickedButton.parentElement.parentElement.insertAdjacentElement(
-                  "afterend",
-                  row
-                );
-              });
-              isDataVisible = true;
-            });
-          });
         })
         .catch((error) => console.log(error));
     })
@@ -210,8 +196,6 @@ function updateTable(pageIndex) {
 updateTable(pageIndex);
 
 // pagination
-
-// let linkElements = document.getElementsByClassName("link");
 const prevBtnElement = document.querySelector(".prev-btn");
 const nextBtnElement = document.querySelector(".next-btn");
 const middleButtonElement = document.querySelector(".middle-btn");
@@ -236,13 +220,6 @@ function getPageNumber(pageIndex) {
 }
 
 prevBtnElement.addEventListener("click", () => {
-  // if ((paginationInputElement.value = 0)) {
-  //   pageIndex = Math.ceil(quantityOfCompanies / 10) * 10 - 9;
-  //   console.log("Pageindex:" + pageIndex);
-  //   updateTable(pageIndex);
-  //   getPageNumber(pageIndex);
-  // }
-
   if (pageIndex === 11) {
     pageIndex = pageIndex - 10;
     updateTable(pageIndex);
@@ -257,13 +234,12 @@ prevBtnElement.addEventListener("click", () => {
   } else {
     getPageNumber(1);
     pageIndex = Math.ceil(quantityOfCompanies / 10) * 10 - 9;
-    console.log("Pageindex:" + pageIndex);
     updateTable(pageIndex);
   }
 });
 
 nextBtnElement.addEventListener("click", () => {
-  if (pageIndex === 81) {
+  if (pageIndex === 71) {
     pageIndex = 1;
     updateTable(pageIndex);
     paginationInputElement.value = 1;
@@ -272,12 +248,10 @@ nextBtnElement.addEventListener("click", () => {
 
   if (pageIndex < Math.floor(quantityOfCompanies / 10) * 10 + 1) {
     pageIndex += 10;
-    console.log("Page index kleiner: " + pageIndex);
     updateTable(pageIndex);
     getPageNumber(pageIndex);
   } else {
     pageIndex = 1;
-    console.log("Page index gleich: " + pageIndex);
     updateTable(pageIndex);
     getPageNumber(pageIndex);
   }
@@ -294,22 +268,18 @@ middleButtonElement.addEventListener("click", () => {
 
   if (paginationInputElementValue === 0) {
     alert("Die erste Seite hat die Nummer 1!");
-
-    console.log("Page index kleiner: " + pageIndex);
-
-    // updateTable(1);
-    // paginationInputElement.value = 1;
     location.reload();
     return;
   }
 
   if (
     paginationInputElementValue >
-    Math.ceil(quantityOfCompanies / quantityOfCompaniesPerSite)
+      Math.ceil(quantityOfCompanies / quantityOfCompaniesPerSite) ||
+    paginationInputElementValue < 0
   ) {
     maxPageNumber = Math.ceil(quantityOfCompanies / quantityOfCompaniesPerSite);
     alert(`Es gibt nur ${maxPageNumber} Seiten!`);
-    paginationInputElement.value = maxPageNumber;
+    location.reload();
     return;
   }
   updateTable(pageIndex);
